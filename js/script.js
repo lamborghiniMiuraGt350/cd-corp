83fa9fed7b88aa12959d0943bb9d87d6f80daec3
lamborghiniMiuraGt350 <uc4iha.sora@gmail.com>
2025-07-30 12:58:25 -0700

    first

---
+++ js/script.js
@@ -0,0 +1,47 @@
+'use strict'
+function ibg() {
+
+    let ibg = document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+        }
+    }
+}
+
+ibg();
+
+
+$('.header-menu__icon').click(function () {
+    $(this).toggleClass('active');
+    $('.header-menu').toggleClass('active');
+    if ($(this).hasClass('active')) {
+        $('body').data('scroll', $(window).scrollTop())
+    }
+    $('body').toggleClass('lock');
+    if (!$(this).hasClass('active')) {
+        $('body, html').scrollTop(parseInt($('body').data('scroll')))
+    }
+})
+
+
+
+
+
+
+window.addEventListener('scroll', function () {
+    scrollY > 0 ? document.querySelector('.header').classList.add('scroll') : document.querySelector('.header').classList.remove('scroll');
+})
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.page-next',
+        prevEl: '.page-prew',
+    },
+});
+
+//__________________________________________________
